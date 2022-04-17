3e9d4d0453b47ee5f818c40deb6485a8aa8d7c24
raphael <raphael.bichet@yahoo.com>
2022-04-17 21:04:56 +0200

    creation de la page formulaire new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def new
+    @famille = Famille.find(params[:famille_id])
+    @item = Item.new
+  end
+
+  def create
+    @famille = Famille.find(params[:famille_id])
+    @item = Item.create(item_params)
+    @item.famille = @famille
+    @item.save
+    redirect_to famille_item_path(@famille, @item)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price)
+  end
 
 end
