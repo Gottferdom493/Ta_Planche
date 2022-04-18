4bf6a88e0c9d8be10a53a05749b0e7ea950c0217
raphael <raphael.bichet@yahoo.com>
2022-04-18 16:02:31 +0200

    ajout du lien vers la page d'achat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_112703) do
+ActiveRecord::Schema.define(version: 2022_04_18_135308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_04_18_112703) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "url_image"
+    t.text "url_achat"
     t.index ["famille_id"], name: "index_items_on_famille_id"
   end
 
