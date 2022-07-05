868d69566ede2776d563f62fadeeb1eb24176b14
raphael <raphael.bichet@yahoo.com>
2022-07-05 13:06:00 +0200

    import trois photo dans new et ajout dans le carousel de show

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   validates :name, :price, presence: true
   belongs_to :famille
 
-  has_one_attached :photo
+  has_one_attached :photo_1;
+  has_one_attached :photo_2;
+  has_one_attached :photo_3;
+
+  # has_many_attached :photo
 
 end
