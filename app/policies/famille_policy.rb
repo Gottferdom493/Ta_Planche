3d211172eec2c0898edc326f763bf09c88593b03
raphael <raphael.bichet@yahoo.com>
2022-07-08 11:43:52 +0200

    ajout des droits pour tous sur new_famille

--- app/policies/famille_policy.rb
+++ app/policies/famille_policy.rb
@@ -1,8 +1,13 @@
 class FamillePolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
   end
+
+  def new?
+    true
+  end
+
 end
