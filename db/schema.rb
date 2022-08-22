e194f3c4946e89f2f91bd978b94fc9bf3a33481c
raphael <raphael.bichet@yahoo.com>
2022-08-22 16:01:00 +0200

    modification dans la db sur la table items adresse -> address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_104651) do
+ActiveRecord::Schema.define(version: 2022_08_22_135040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2022_08_22_104651) do
     t.text "detail"
     t.bigint "user_id"
     t.string "matiere"
-    t.string "adresse"
+    t.string "address"
     t.float "latitude"
     t.float "longitude"
     t.index ["famille_id"], name: "index_items_on_famille_id"
