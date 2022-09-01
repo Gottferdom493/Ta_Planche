500511343a723db604654a0d12776743fb36730b
raphael <raphael.bichet@yahoo.com>
2022-09-01 18:01:42 +0200

    création de la page index pour la table profil

--- app/controllers/profils_controller.rb
+++ app/controllers/profils_controller.rb
@@ -1,2 +1,10 @@
 class ProfilsController < ApplicationController
+
+
+  def index
+    @profils = Profil.all
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'familles#index'
+  
   resources :familles do
     resources :items
   end
-  resources :items do
-    resources :profils
+
+  resources :profils do
+    resources :items
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
