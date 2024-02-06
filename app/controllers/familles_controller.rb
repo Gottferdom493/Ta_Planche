ede33eff4adafac2c89236463510e65cb244fd3e
raphael <raphael.bichet@yahoo.com>
2024-02-06 09:24:07 +0100

    filtre pour taille min et max, qui ne fonctionne pas pour le moment

--- app/controllers/familles_controller.rb
+++ app/controllers/familles_controller.rb
@@ -7,6 +7,7 @@ class FamillesController < ApplicationController
     @familles = Famille.all
     @familles = Famille.includes(:items)
 
+    # Filtrer pour le type de Famille
     if params[:filter].present?
       famille_ids = params[:filter][:familles]
 
@@ -18,8 +19,16 @@ class FamillesController < ApplicationController
       max_price = params[:filter][:max_price].to_i
 
       @familles = @familles.joins(:items).where(items: { price: min_price..max_price })
-    end
+      # Filtrer par taille minimale
+      if params[:filter][:min_size].present?
+        @familles = @familles.joins(:items).where("items.size >= ?", params[:filter][:min_size])
+      end
 
+      # Filtrer par taille maximale
+      if params[:filter][:max_size].present?
+        @familles = @familles.joins(:items).where("items.size <= ?", params[:filter][:max_size])
+      end
+    end
   end
 
   def show
