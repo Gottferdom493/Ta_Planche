d045361acdd5a6bf403f1b4690c3daea98a1e49b
raphael <raphael.bichet@yahoo.com>
2024-02-07 16:29:28 +0100

    ajout des information par default pour les champs nom et prenom

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -6,4 +6,17 @@ class Profil < ApplicationRecord
   has_one_attached :img_profil;
 
 
+  before_validation :set_default_name
+
+  validates :date_naissance, presence: true
+
+  # information ajouté par default
+
+  private
+
+  def set_default_name
+    self.nom = "info manquante" if nom.blank?
+    self.prenom = "info manquante" if prenom.blank?
+  end
+
 end
